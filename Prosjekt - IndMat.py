fe8b99505d8958a8af19e54a4b0d8875cbe160cc
Lasse Sagnes Tomsson <Lsagnes02@gmail.com>
2023-02-22 13:23:41 +0100

    Lagt til trunkeringsfunksjon, jf. oppg. 1b

--- Prosjekt - IndMat.py
+++ Prosjekt - IndMat.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+#Funksjonar
+def truncSVD(U,S,Vt,d):
+    return U[:,:d], S[:d,:d], Vt[:d]
+
 #Testvektorar oppgåve 1
 A1 = np.array([[1000,1],
                [0, 1],
@@ -12,6 +16,5 @@ b2 = np.array([0,0,1],dtype=float)
 b3 = np.array([0,1,0],dtype=float)
 B = np.vstack((b1,b2,b3))
 
-U,S,V = np.linalg.svd(A1, full_matrices=False); V=V.T; S = np.diag(S)
-
-#Funksjonar
+U,S,Vt = np.linalg.svd(A2, full_matrices=False); S = np.diag(S)
+Ud, Sd, Vtd = truncSVD(U,S,Vt,2)
