a9f4dac6f4d31cdd11da4fb80529fe7d0e8d7574
Lasse Sagnes Tomsson <Lsagnes02@gmail.com>
2023-02-22 12:48:28 +0100

    Lagt til testvektorar og SVD for oppg. 1a

--- Prosjekt - IndMat.py
+++ Prosjekt - IndMat.py
@@ -1,3 +1,17 @@
 import numpy as np
-print('Hello World!')
-print('Test')
+
+#Testvektorar oppgåve 1
+A1 = np.array([[1000,1],
+               [0, 1],
+               [0, 0]],dtype=float)
+A2 = np.array([[1,0,0],
+               [1,0,0],
+               [0,0,1]],dtype=float)
+b1 = np.array([2,1,0],dtype=float)
+b2 = np.array([0,0,1],dtype=float)
+b3 = np.array([0,1,0],dtype=float)
+B = np.vstack((b1,b2,b3))
+
+U,S,V = np.linalg.svd(A1, full_matrices=False); V=V.T; S = np.diag(S)
+
+#Funksjonar
