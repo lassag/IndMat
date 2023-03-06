0e11884d72947f7d81d005dc7610b6c5a688bb1c
Lasse Sagnes Tomsson <Lsagnes02@gmail.com>
2023-03-06 14:57:34 +0100

    Gjort diverse endringar med test-data

--- Prosjekt - IndMat.py
+++ Prosjekt - IndMat.py
@@ -38,30 +38,36 @@ def dist(P,b):
 
 def checkalld(A,b,projectiontype="orth"):
     U, S, Vt = np.linalg.svd(A, full_matrices=False)
-    D = np.zeros(pixels)
+    print(A.shape)
+    print(b.shape)
     if projectiontype == "orth":
+        D = np.zeros(b.shape)
         for i in range(pixels):
             Ud, Sd, Vtd = truncSVD(U,S,Vt,i)
             P = orthproj(Ud,b)
             D[i] = dist(P,b)
     elif projectiontype == "nn":
-        for i in range(pixels):
+        D = np.zeros((A.shape[1],b.shape[1]))
+        for i in range(A.shape[1]):
             Ud, Sd, Vtd = truncSVD(U,S,Vt,i)
             P = nnproj(Ud,b,i)
             D[i] = dist(P,b)
     plt.semilogy(D)
     plt.show()
+    
+def generatetest(t):
+    print(test)
+    return np.swapaxes(np.swapaxes(test[:,:,np.random.choice(test.shape[2], size = t, replace = False)],0,1),1,2)
 
 def getclasses(k,d):
-    C = np.swapaxes(train[:,:10,:k],0,1)
+    C = np.swapaxes(train[:,:,:k],0,1)
     W = np.zeros((10,pixels,d))
     H = np.zeros((10,d,k))
     for i in range(10):
         W[i], H[i] = WHfact(C[i],d)
     return C, W, H
 
-def classify(k, d, t=800, projectiontype="orth", maxiter = 75, delta = 10e-2):
-    B = np.swapaxes(np.swapaxes(test[:,:,np.random.choice(test.shape[2], size = t, replace = False)],0,1),1,2)
+def classify(B, k, d, t=800, projectiontype="orth", maxiter = 75, delta = 10e-2):
     C, W, H = getclasses(k,d)
     P = np.zeros((10,10,pixels,t))
     D = np.zeros((10,10,t))
@@ -74,7 +80,7 @@ def classify(k, d, t=800, projectiontype="orth", maxiter = 75, delta = 10e-2):
                 P[j,i] = nnproj(C[j],B[i].T,d, maxiter, delta)
             D[i,j] = dist(P[j,i],B[i].T)
     
-    return B, P, D, np.argmin(D, axis=0)
+    return P, D, np.argmin(D, axis=0)
 
 def display(B,P,D,C,c,r):
     plt.imshow(B[c,r].reshape((28,28)), cmap = 'gray')
@@ -114,14 +120,15 @@ def showaccuracy(C,t=800,indecies=[0,1,2,3,4,5,6,7,8,9]):
     print(f'Total treffsikkerheit: {np.round(np.sum(A)/A.size * 100,1)}%')
 
 def getaccuracies(k, n, t=800, projectiontype="orth",maxiter=75,delta=10e-2):
+    B = generatetest(t)
     Q = np.ones(n,dtype = int)*2
     acc = np.zeros(n)
     for i in range(n):
         Q[i] = Q[i]**i
         if projectiontype == "orth":
-            B, P, D, C = classify(k,Q[i],t,"orth")
+            P, D, C = classify(B, k,Q[i],t,"orth")
         elif projectiontype == "nn":
-            B, P, D, C = classify(k,Q[i],t,"nn",maxiter,delta)
+            P, D, C = classify(B, k,Q[i],t,"nn",maxiter,delta)
         acc[i] = accuracy(C,t,indecies)
     
     plt.semilogx(Q, acc, base=2, subs=None)
@@ -200,31 +207,35 @@ def plotimgs(imgs, nplot = 4):
 #########################################
 
 #Klassifisering
-k = 1000 #Tal på treningsdatapunkt
+k = 500 #Tal på treningsdatapunkt
 n = 10 #Tal på toarpotensar 
-t = 100 #Tal på testdatapunkt
+t = 50 #Tal på testdatapunkt
 dorth = 2**5 #Trunkeringskoeffisient
 dnn = 2**9 #Utval ENMF
-c = 4 #Klasse for test
+c = 6 #Klasse for test
 r = 0 #Nummer for test
 maxiter = 75
 delta = 10e-2
-indecies = np.array([0,1,2,3,4,5,6,7,8,9])
+indecies = [0,1,2,3,4,5,6,7,8,9]
 
 # A, W, H = getclasses(k,d)
 # U, S, Vt = np.linalg.svd(A[c], full_matrices=False)
 # plt.semilogy(S)
 # plt.show()
 
-B, P, D, C = classify(k, dorth, t, "orth")
-display(B, P, D, C, c, r)
-showaccuracy(C,t,indecies)
-getaccuracies(k,n,t,"orth",maxiter,delta)
+B = generatetest(t)
+print(B)
 
-B, P, D, C = classify(k, dnn, t, "nn", maxiter, delta)
-display(B, P, D, C, c, r)
-showaccuracy(C,t,indecies)
-getaccuracies(k,n,t,"nn",maxiter,delta)
+# P, D, C = classify(B, k, dorth, t, "orth")
+# display(B, P, D, C, c, r)
+# showaccuracy(C,t,indecies)
+# getaccuracies(k,n,t,"orth",maxiter,delta)
+
+# P, D, C = classify(B, k, dnn, t, "nn", maxiter, delta)
+# display(B, P, D, C, c, r)
+# showaccuracy(C,t,indecies)
+# getaccuracies(k,n,t,"nn",maxiter,delta)
+
 
 ###################################
 
